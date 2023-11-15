9b85cf24bb32ecdc61a62e1d5eed07b37d2f681f
ryansatriia <ryan.satria100@gmail.com>
2023-11-15 15:27:53 +0700

    nambahin button-button deposit

---
+++ lib/Deposit/debit.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class debitPage extends StatelessWidget {
+  const debitPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Withdraw'),
+        backgroundColor: Color.fromARGB(255, 147, 76, 175), // Warna tema e-money
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Withdraw Amount',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter amount',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement withdrawal logic
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(255, 147, 76, 175), // Warna tema e-money
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  'Withdraw',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
