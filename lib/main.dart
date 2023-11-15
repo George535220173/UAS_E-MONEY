a854ca7cd991f021e9017ecf886685b260b14b51
George535220173 <george.535220173@stu.untar.ac.id>
2023-11-15 17:40:58 +0700

    update register

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:uas_emoney/registerlogin/login.dart';
 import 'Home/home.dart';
 
-void main(){
+
+void main() {
   runApp(const myApp());
 }
 
 class myApp extends StatelessWidget {
+  
   const myApp({Key? key}) : super(key: key);
 
   @override
