607c0e3470d69874e18c1bb1e096401adb78c5ad
George535220173 <george.535220173@stu.untar.ac.id>
2023-11-15 14:16:25 +0700

    fixed overflowing keyboard saat mau masukan recipient dan amount

--- lib/Transfer/transfer.dart
+++ lib/Transfer/transfer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; 
+import 'package:flutter/services.dart';
 
 class TransferPage extends StatelessWidget {
   const TransferPage({Key? key}) : super(key: key);
@@ -9,82 +9,81 @@ class TransferPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Transfer'),
-        backgroundColor: Color.fromARGB(255, 147, 76, 175), 
+        backgroundColor: Color.fromARGB(255, 147, 76, 175),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 40,
-              backgroundColor: Color.fromARGB(255, 147, 76, 175),
-              child: Icon(
-                Icons.account_circle,
-                size: 60,
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Recipient',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter recipient username or ID',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Amount',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              keyboardType: TextInputType.number, 
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly 
-              ],
-              decoration: InputDecoration(
-                hintText: 'Enter Amount',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(255, 147, 76, 175), 
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.send),
-                    SizedBox(width: 10),
-                    Text(
-                      'Transfer',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Color.fromARGB(255, 147, 76, 175),
+                child: Icon(
+                  Icons.account_circle,
+                  size: 60,
+                  color: Colors.white,
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Text(
+                'Recipient',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter recipient username or ID',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Amount',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: InputDecoration(
+                  hintText: 'Enter Amount',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 147, 76, 175),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.send),
+                      SizedBox(width: 10),
+                      Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
